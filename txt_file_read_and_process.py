b227b676e26914faff5707eef9b4af6032561e9a
ZuoYuShen <625080891@qq.com>
2019-12-13 17:07:55 +0800

    useful in some dataset processing

--- txt_file_read_and_process.py
+++ txt_file_read_and_process.py
@@ -10,7 +10,7 @@ eng_set = 'train.en.txt'
 de_set = 'train.de.txt'
 target_set = 'eng-de.txt'
 with open(path + eng_set, 'r', encoding = 'utf-8') as f:
-    line_eng = [line.strip() for line in f.readlines()]
+    line_eng = [line.strip() for line in f.readlines()]    #读取字符并去掉其中的‘\n’.字符串才有strip方法,当里面不写值时，默认删除空白符（包括'\n', '\r', '\t', ' ')
     #line_eng = f.readlines().split("\n")
 f.close()
 
@@ -22,10 +22,10 @@ g.close()
 new_set_list = []
 
 for i in range(len(line_eng)-1):
-    new_set_list.append(line_eng[i]+'\t'+line_de[i])
+    new_set_list.append(line_eng[i]+'\t'+line_de[i])     #构建新list并入两个数据集的信息
 
 with open(path + target_set, 'w', encoding = 'utf-8') as f_target:
     for item in new_set_list:
-        f_target.write("%s\n" % item)
+        f_target.write("%s\n" % item)                    #输出为txt文件
 
 f_target.close()
